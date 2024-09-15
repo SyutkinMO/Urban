02c7c1c14b5e14f51bfe24d1296f7bc249604f2f
Maxim <syutkin.mo@gmail.com>
2024-09-15 15:01:31 +0500

    Module_1_pep

--- Lesson1.py
+++ Lesson1.py
@@ -1,6 +1,6 @@
 example = "топинамбур"
-print (example[0])
-print (example[-1])
-print (example[5:])
-print (example[::-1])
-print (example[1::2])
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
